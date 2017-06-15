574c3acd3c9022f780b06e9584936c347a2c1b4e
Ilya Bashilov <tango1603@yandex.ru>
2017-06-15 16:15:12 +0300

    Add project files.

---
+++ AppTest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppTest.Models;
+
+namespace AppTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        AddressContext db = new AddressContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Address> addreses = db.Addreses;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Addreses = addreses;
+            // возвращаем представление
+            return View();
+        }
+
+    }
+}
---
+++ AppTest/Models/AddressDbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppTest.Models
+{
+    public class AddressDbInitializer
+    {
+
+    }
+}
